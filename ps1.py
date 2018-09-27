e006ece15141f0aa124ffc4d277e278b3e337df0
2018JTM2244 <hiteshjtm182244@gmail.com>
2018-09-27 21:09:47 +0530

    final

--- ps1.py
+++ ps1.py
@@ -2,5 +2,94 @@
 
 ####### write your code here ##########
 
+def main():
+	n,m=int(input("Enter the integer n: ")), int(input("Enter the integer m: "))
+	if not (n>1 and n<=105):
+		return
+	if not (m>1 and m<=105):
+		return
 
 
+	l= []
+	listofmin=[]
+	for x in range(n):
+		s= list(input())
+		l=l+[s]
+	print(l)
+
+	i=0 ; j=0
+#	a='S'|'D'
+	countr,countl,countd,countu=0,0,0,0
+	for a in l[i][j]:
+		for a in  l[i][j]:
+			while l[i][j]=='S':
+				countr+=1
+				if j==m-1:
+					break
+				j+=1
+			j=j-countr
+			while l[i][j]=='S':
+				countl+=1
+				if j==-1:
+					break
+				j-=1
+			j=j+countl
+			while l[i][j]=='S':
+				countu+=1
+				if i==-1:
+					break
+				i-=1
+			i=i+countu
+			while l[i][j]=='S':
+				countd+=1
+				if i==n-1:
+					break
+			i=i-countd
+			j+=1		
+		i+=1
+		listofmin=listofmin+[min(countr,countl,countd,countu)]
+		print(listofmin)
+		if i==n-1:
+			break
+
+
+	max_value=max(listofmin)
+	if max_value>1:
+		print(max_value+" "+max_value)
+	else:
+		temp = max_value
+		listofmin.remove(max_value)
+		max_value=max(listofmin)
+		print(temp+" "+max_value)
+
+
+
+
+#		for S in l[i][j]
+#			i-=1
+#			while l[i]==S
+#				countl+=1
+#		i=i+countl			
+#		while l[i][j-1]
+#			while l[i-=1][j]
+#
+#	import re
+#
+#	for S in l
+#
+#	maxpattern = 0
+#	if (m>)
+#
+# i=0
+# count=0
+# while i<n
+# 	for S in list[i][:1]
+# 		if count==1
+# 			break
+# 		count+=1
+# 	for S in list[i][1:2]
+# 		for S in list[i+1][:j]
+		
+
+if __name__ == '__main__':
+	main()
--- ps2.py
+++ ps2.py
@@ -1,37 +1,111 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+#Main function
+def main():
+	#key inputs
+	k1,k2,k3=int(input("Enter the k1 key: ")), int(input("Enter the k2 key: ")), int(input("Enter the k3 key: "))
+	if not (k1>0 and k1<=150):
+		return
+	if not (k2>0 and k2<=150):
+		return
+	if not (k3>0 and k3<=150):
+		return
 
-k1,k2,k3=int(input("Enter the k1 key: ")), int(input("Enter the k2 key: ")), int(input("Enter the k3 key: "))
-S= raw_input("Enter the string required to be decrypted: ")
-print S
-length=len(S)
+	#String input
+	S= input("Enter the string required to be decrypted: ")
+	print(S)
+	length=len(S)
 
-group1=['a','b','c','d','e','f','g','h','i']
-group2=['j','k','l','m','n','o','p','q','r']
-group3=['s','t','u','v','w','x','y','z','_']
+	if len(S)>150:
+		exit()
 
-list1=[]
-list2=[]
-list3=[]
+	#groups given in the question
+	group1=['a','b','c','d','e','f','g','h','i']
+	group2=['j','k','l','m','n','o','p','q','r']
+	group3=['s','t','u','v','w','x','y','z','_']
 
-i=0
-j,k,m=0,0,0
+	list1=[]
+	list2=[]
+	list3=[]
+	reqlist=[]
 
-while i<len(S):
-	if S[i] in group1:
-		list1[j]=S[i]
-		j+=1
-        
-	if S[i] in group2:
-		list1[j]=S[2]
-		m+=1
+	i=0
+	j,k,m=0,0,0
 
-	if S[i] in group2:
-		list1[j]=S[2]
-		k+=1
-    
+	#loop for separing the string into lsits according to groups defined
+	while i<len(S):
+		if S[i] in group1:
+			list1=list1+[S[i]]
+			j+=1
+	        
+		if S[i] in group2:
+			list2=list2+[S[i]]
+			k+=1
 
-print list1
-print list2
-print list3
+		if S[i] in group3:
+			list3=list3+[S[i]]
+			m+=1
+
+		i+=1
+
+	#function definition for rotation of elements in the grouped lists
+	def rightRotate(l, n):
+	    return l[-n:] + l[:-n]
+
+
+	list1 = rightRotate(list1,k1)
+	list2 = rightRotate(list2,k2)
+	list3 = rightRotate(list3,k3)
+
+	i,j,k,m=i-i,j-j,k-k,m-m
+
+	#loop for converting enrypted message into decrypted one
+	while i<len(S):
+		if S[i] in group1:
+			reqlist=reqlist+[list1[j]]
+			j+=1
+	        
+		if S[i] in group2:
+			reqlist=reqlist+[list2[k]]
+			k+=1
+
+		if S[i] in group3:
+			reqlist=reqlist+[list3[m]]
+			m+=1
+
+		i+=1
+
+	s= ''.join(reqlist) 
+	print(s)
+
+if __name__ == '__main__':
+	main()
+#import numpy
+#numpy.roll(list1,k1)
+#numpy.roll(list2,k2)
+#numpy.roll(list3,k3)
+
+#import collections
+#list1.rotate(k1)
+#list2.rotate(k2)
+#list3.rotate(k3)
+
+#function for rotating the list elements
+#def rotate(list,x):
+#	x=x%len(list)
+#	return list[x:] + list[:x]
+
+# Returns the rotated list 
+# def rightRotate(lists,num): 
+#     output_list = [] 
+#     # Will add values from n to the new list 
+#     for item in range(len(lists) - num, len(lists)): 
+#         output_list.append(lists[item])   
+#     # Will add the values before 
+#     # n to the end of new list     
+#     for item in range(0, len(lists) - num):  
+#         output_list.append(lists[item])     
+#     return output_list 
+
+#function for rotating the list elements
