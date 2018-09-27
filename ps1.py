dbb1d3ce6d605bcbbbc48dad3bf4b3466481a3ed
2018JTM2244 <hiteshjtm182244@gmail.com>
2018-09-27 14:24:49 +0530

    Started with second problem statement

--- ps1.py
+++ ps1.py
@@ -1,3 +1,6 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+
+
--- ps2.py
+++ ps2.py
@@ -1,3 +1,37 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+
+k1,k2,k3=int(input("Enter the k1 key: ")), int(input("Enter the k2 key: ")), int(input("Enter the k3 key: "))
+S= raw_input("Enter the string required to be decrypted: ")
+print S
+length=len(S)
+
+group1=['a','b','c','d','e','f','g','h','i']
+group2=['j','k','l','m','n','o','p','q','r']
+group3=['s','t','u','v','w','x','y','z','_']
+
+list1=[]
+list2=[]
+list3=[]
+
+i=0
+j,k,m=0,0,0
+
+while i<len(S):
+	if S[i] in group1:
+		list1[j]=S[i]
+		j+=1
+        
+	if S[i] in group2:
+		list1[j]=S[2]
+		m+=1
+
+	if S[i] in group2:
+		list1[j]=S[2]
+		k+=1
+    
+
+print list1
+print list2
+print list3
